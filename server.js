576ed31a09e7498a604f3e5b4b9911b7d50bb9f1
Kazi Mamun <kazi299499@gmail.com>
2021-04-26 08:03:56 +0600

    environment variable added

--- server.js
+++ server.js
@@ -4,25 +4,28 @@ const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+const port = process.env.PORT || 4000;
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g1juc.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`
+
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+require('dotenv').config()
 
 
 app.get('/',(req,res)=>{
     res.json({
         message : 'welcome to our application'
     })
-})
+});
 
-const port = process.env.PORT || 4000;
-const mongoUri = 'mongodb+srv://mern:mern123@cluster0.g1juc.mongodb.net/mernDb?retryWrites=true&w=majority'
+
 app.listen(port,()=>{
     console.log(`server is running at port ${port}`);
     mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true },()=>{
         console.log('database connected');
     })
-})
+});
